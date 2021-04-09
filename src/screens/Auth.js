c0925a68676a66ce8e7baef35c840d0c6864c623
Nilton Santos <niltonsantosmf2@gmail.com>
2021-04-09 16:17:40 -0300

    Icones para a tela de login e cadastro

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -3,9 +3,7 @@ import { ImageBackground,
     Text,
     StyleSheet, 
     View,
-    TextInput,
     TouchableOpacity,
-    Platform, 
     Alert
 
 } from 'react-native'
@@ -13,6 +11,8 @@ import { ImageBackground,
 import backgroundImage from '../../assets/imgs/login.jpg'
 
 import comonStyles from '../comonStyles'
+
+import AuthInput from '../components/AuthInput'
 
 export default class Auth extends Component {
 
@@ -42,18 +42,21 @@ export default class Auth extends Component {
                         {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
                     </Text>
                     {this.state.stageNew && 
-                        <TextInput placeholder="Nome" value={this.state.name}
+                        <AuthInput icon='user' placeholder="Nome"
+                         value={this.state.name}
                         style={styles.input}
                         onChangeText={name => this.setState({ name })} />
                     }
-                <TextInput placeholder="Email" value={this.state.email}
+                <AuthInput icon='at' placeholder="Email" 
+                        value={this.state.email}
                         style={styles.input}
                         onChangeText={email => this.setState({ email })} />
-                    <TextInput placeholder="Senha" value={this.state.password}
+                    <AuthInput icon='lock' placeholder="Senha" 
+                        value={this.state.password}
                         style={styles.input} secureTextEntry = { true }
                         onChangeText={password => this.setState({ password })} />
                     {this.state.stageNew &&
-                        <TextInput placeholder="Confirmação de Senha"
+                        <AuthInput icon='asterisk' placeholder="Confirmação de Senha"
                         value={this.state.confirmPassword}
                         style={styles.input} secureTextEntry = { true }
                         onChangeText={confirmPassword => this.setState({ confirmPassword })} />
@@ -105,13 +108,13 @@ const styles = StyleSheet.create({
     input: {
         marginTop: 10,
         backgroundColor: '#FFF',
-        padding: Platform.OS == 'ios' ? 15 : 10
     },
     button: {
         backgroundColor: '#080',
         marginTop: 10,
         padding: 10,
         alignItems: 'center',
+        borderRadius: 7
     },
     buttonText: {
         fontFamily: comonStyles.fontFamily,
